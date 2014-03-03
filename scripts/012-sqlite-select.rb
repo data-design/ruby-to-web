a42923f0c5af86a8802ad39dd7542a196d0bc3f4
Dan Nguyen <dansonguyen@gmail.com>
2014-03-03 18:56:08 -0500

    scripts

---
+++ scripts/012-sqlite-select.rb
@@ -0,0 +1,8 @@
+#### Let's query our database
+# Prereqs: In our `helper.rb` file, we now have a constant set up like so:
+# 
+# We use `open`, which is a *class* method of the `File` class 
+# and pass in two `String` arguments:
+#
+# 1. The file path
+# 2. The *mode*, in this case `'r'`
--- scripts/helper.rb
+++ scripts/helper.rb
@@ -1,3 +1,5 @@
+require 'pathname'
+
 LEGISLATORS_FILE = File.expand_path '../../data/legislators.csv', __FILE__
 
 # after scripts/008-csv.rb
@@ -5,7 +7,5 @@ DATA_DIR = Pathname.new File.expand_path('../../data', __FILE__)
 RESULTS_DIR = Pathname.new File.expand_path('../../results', __FILE__)
 TWITTER_LEGISLATORS_FILE = RESULTS_DIR.join 'legislators-on-twitter.csv'
 
-
-
-#TWEET_CONGRESS_DB_NAME = File.expand_path '../../results/tweet-congress.sqlite', __FILE__
+TWEET_CONGRESS_DB_NAME = File.expand_path '../../results/tweet-congress.sqlite', __FILE__
 #TWEET_CONGRESS_HEADERS = ['title', 'firstname', 'lastname', 'party', 'state', 'district', 'bioguide_id', 'birthdate', 'twitter_id']
